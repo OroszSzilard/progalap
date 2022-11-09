f02748a76ea0a9b3a311c1169eb8ffec2a96779a
OroszSzilard <oroszszilard05@gmail.com>
2022-11-09 15:17:14 +0100

    Update gyak6_1.c

--- gyak6_1.c
+++ gyak6_1.c
@@ -16,8 +16,12 @@ int main()
 {
     Datum d1, d2;
 
-    printf("Első dátum:\n"); beolvas(&d1);
-    printf("Második dátum:\n"); beolvas(&d2);
+    printf("Első dátum:\n"); 
+    //beolvas(&d1);
+    beolvasFormazott(&d1)
+    printf("Második dátum:\n"); 
+    //beolvas(&d2);
+    beolvasFormazott(&d2)
 
     Datum regi = regebbi_datum(d1, d2);
     kiir(regi);
@@ -50,9 +54,38 @@ Datum regebbi_datum(Datum d1, Datum d2)
         else return d1;
         }
         else if(d1.ho>d2.ho) return d2;
-        
+
         else return d1;
     }
     else if (d1.ev>d2.ev) return d2;
     else return d1;
 }
+void beolvasFormazott(Datum *d)
+{
+    char str[12];
+    int ok;
+    do {
+        ok = 1;
+        scanf("%s", str);
+        if (sscanf(str, "%d.%d.%d.", &d->ev, &d->honap, &d->nap) != 3) 
+        {
+            printf("Hibás a dátum!\n");
+            ok = 0;
+        }
+        } 
+    while (!ok);
+    return ;
+}
+
+int HanyEves(Datum d) 
+{
+    char ho[3];
+    int ev, nap;
+    sscanf(__DATE__, "%s %d %d", ho, &nap, &ev);
+    printf("Jelenlegi datum: %s", __DATE__);
+
+
+
+    return ev-d.ev;
+}
+
